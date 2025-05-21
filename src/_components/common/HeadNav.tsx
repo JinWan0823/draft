25ef6c487a8c597e1d60047f6e587f8d543a4b22
jinWan0823 <dhksl823@gmail.com>
2025-05-21 20:43:19 +0900

    💄 메인페이지 레이아웃 구성

--- src/_components/common/HeadNav.tsx
+++ src/_components/common/HeadNav.tsx
@@ -1,8 +1,16 @@
 export default function HeadNav() {
   return (
     <header className="h-[60px] bg-[#333]">
-      <div className="inner w-[1240px] mx-auto">
-        <p>dfd</p>
+      <div className="inner w-[1240px] h-[60px] mx-auto flex justify-between items-center">
+        <p>DRAFT</p>
+
+        <nav>
+          <ul className="flex">
+            <li className="font-bold">리더등록</li>
+            <li className="font-bold">선수등록</li>
+            <li className="font-bold">드래프트</li>
+          </ul>
+        </nav>
       </div>
     </header>
   );
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,20 @@
 export default function Home() {
-  return <p className="font-bold">프로젝트 시작2</p>;
+  return (
+    <section className="w-[1240px] mx-auto py-[120px]">
+      <h2 className="text-center font-bold text-4xl">Today Draft</h2>
+
+      <div className="flex flex-col items-center justify-center mt-[40px] w-full">
+        <input type="text" className="border-2 mb-6 w-full max-w-[400px]" />
+
+        <ul className="grid grid-cols-3 gap-6 w-full">
+          <li className="h-40 bg-gray-200 rounded-md"></li>
+          <li className="h-40 bg-gray-200 rounded-md"></li>
+          <li className="h-40 bg-gray-200 rounded-md"></li>
+          <li className="h-40 bg-gray-200 rounded-md"></li>
+          <li className="h-40 bg-gray-200 rounded-md"></li>
+          <li className="h-40 bg-gray-200 rounded-md"></li>
+        </ul>
+      </div>
+    </section>
+  );
 }
