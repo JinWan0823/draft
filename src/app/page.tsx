44bb2ab9e69e56cdca7ef53c80135d9ba0b8a11e
jinWan0823 <dhksl823@gmail.com>
2025-05-28 22:21:13 +0900

    💄 메인페이지 리스트,애니메이션 추가

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,5 @@
 import Logo from "@/_components/common/Logo";
+import MenuList from "@/_components/common/MenuList";
 import SearchBar from "@/_components/common/SearchBar";
 
 export default function Home() {
@@ -7,13 +8,11 @@ export default function Home() {
       <div className="flex flex-col items-center justify-center mt-[40px] w-full">
         <Logo />
         <SearchBar />
-        <ul className="grid grid-cols-3 gap-6 w-full">
-          <li className="h-40 bg-gray-200 rounded-md"></li>
-          <li className="h-40 bg-gray-200 rounded-md"></li>
-          <li className="h-40 bg-gray-200 rounded-md"></li>
-          <li className="h-40 bg-gray-200 rounded-md"></li>
-          <li className="h-40 bg-gray-200 rounded-md"></li>
-          <li className="h-40 bg-gray-200 rounded-md"></li>
+        <ul className="grid grid-cols-3 gap-8 w-full mt-[20px]">
+          <MenuList category={"선수 목록"} />
+          <MenuList category={"전술보드"} />
+          <MenuList category={"드래프트"} />
+          <MenuList category={"선수 티어메이커"} />
         </ul>
       </div>
     </section>
