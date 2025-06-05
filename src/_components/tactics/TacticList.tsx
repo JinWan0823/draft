d5830feee8dabe4b84be3913e704cd4f49d6c4d5
jinWan0823 <dhksl823@gmail.com>
2025-06-05 22:58:31 +0900

    🎨 플레이어 검색 필터링

--- src/_components/tactics/TacticList.tsx
+++ src/_components/tactics/TacticList.tsx
@@ -1,7 +1,16 @@
+"use client";
+
+import { useState } from "react";
 import { dummyPlayers } from "../../../dumy";
 import TacticPlayer from "./TacticPlayer";
 
 export default function TacticList() {
+  const [search, setSearch] = useState("");
+
+  const filteredPlayers = dummyPlayers.filter((item) =>
+    item.name.includes(search)
+  );
+
   return (
     <div className="p-1 py-2 w-[220px] fixed left-[20px] top-1/2 -translate-y-1/2 bg-white shadow-xl rounded-[20px] border-l-4 border-[#f37812]">
       <p className="text-lg font-bold text-center text-[#f37812]">선수 목록</p>
@@ -9,10 +18,11 @@ export default function TacticList() {
         <input
           placeholder="선수 검색"
           className="w-full border-1 border-gray-300 p-2 py-1 rounded my-2"
+          onChange={(e) => setSearch(e.currentTarget.value)}
         />
       </div>
-      <ul className="max-h-[60vh] overflow-y-scroll custom-scrollbar p-1">
-        {dummyPlayers.map((item, key) => (
+      <ul className="max-h-[60vh] h-[60vh] overflow-y-scroll custom-scrollbar p-1">
+        {filteredPlayers.map((item, key) => (
           <TacticPlayer item={item} key={key} />
         ))}
       </ul>
--- src/_components/tactics/TacticPlayer.tsx
+++ src/_components/tactics/TacticPlayer.tsx
@@ -13,7 +13,10 @@ interface PlayerInfo {
 export default function TacticPlayer({ item }: TacticProps) {
   return (
     <li className="border-gray-200 rounded border-1 my-2 p-2 flex items-center">
-      <div className="thumb w-[44px] h-[44px] rounded-full bg-[#333] mr-2"></div>
+      <div
+        className="thumb w-[44px] h-[44px] rounded-full bg-[#333] mr-2 bg-cover bg-center"
+        style={{ backgroundImage: `url(${item.image})` }}
+      ></div>
       <div>
         <p className="text-sm">{item.name}</p>
         <PositionBadge position={item.position} />
