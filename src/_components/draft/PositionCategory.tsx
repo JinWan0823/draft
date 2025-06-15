f5466b9449fef7ecfd0910ed18f41e45910e94dc
jinWan0823 <dhksl823@gmail.com>
2025-06-16 00:41:34 +0900

    ✨ 드래프트 선수 선택 기능 추가

--- src/_components/draft/PositionCategory.tsx
+++ src/_components/draft/PositionCategory.tsx
@@ -1,11 +1,18 @@
 import { getBadgeColor, renamePosition } from "@/_utill/position";
 import { dummyPlayers } from "../../../dumy";
+import { TeamPlayerProps } from "@/app/(route)/draft/page";
 
 interface PositionProps {
   position: string;
+  handlePlayerSelect: (player: TeamPlayerProps) => void;
+  isAlreadySelected: (playerName: string) => boolean;
 }
 
-export default function PositionCategory({ position }: PositionProps) {
+export default function PositionCategory({
+  position,
+  handlePlayerSelect,
+  isAlreadySelected,
+}: PositionProps) {
   const positionPlayers = dummyPlayers.filter(
     (item) => item.position === position
   );
@@ -24,7 +31,12 @@ export default function PositionCategory({ position }: PositionProps) {
         <ul className="flex flex-wrap mt-3 gap-2">
           {positionPlayers.map((p, idx) => (
             <li key={idx}>
-              <button className="px-2 py-1 border-gray-300 border-1 rounded text-sm">
+              <button
+                onClick={() => handlePlayerSelect(p)}
+                disabled={isAlreadySelected(p.name)}
+                className="px-2 py-1 border-gray-300 border-1 rounded text-sm
+                  disabled:bg-gray-400 disabled:text-white"
+              >
                 {p.name}
               </button>
             </li>
