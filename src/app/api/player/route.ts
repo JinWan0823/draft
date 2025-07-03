ec20c6f68137c07b38895673a7c51725a20dedb4
jinWan0823 <dhksl823@gmail.com>
2025-07-03 17:44:19 +0900

    ✨ 선수 생성 api

--- src/app/api/player/route.ts
+++ src/app/api/player/route.ts
@@ -1,4 +1,5 @@
 import { connectDB } from "@/_lib/mongodb";
+import { NextRequest } from "next/server";
 
 export async function GET() {
   const client = await connectDB;
@@ -14,5 +15,31 @@ export async function GET() {
     return Response.json(players);
   } catch (error) {
     return Response.json({ message: error }, { status: 500 });
+  }
+}
+
+export async function POST(req: NextRequest) {
+  const client = await connectDB;
+  const db = client.db("draft");
+
+  try {
+    const body = await req.json();
+    const { name, position, subPosition, playerInfo, career } = body;
+
+    if (!name || !position) {
+      return Response.json({ message: "필수 항목 누락" }, { status: 400 });
+    }
+
+    const res = await db.collection("player").insertOne({
+      name: name,
+      position: position,
+      subPosition: subPosition,
+      note: playerInfo,
+      achievements: career,
+    });
+
+    return Response.json(res, { status: 201 });
+  } catch (err) {
+    return Response.json({ message: err }, { status: 500 });
   }
 }
