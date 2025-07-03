09017d882c6f33ee79655d2f7cc1bf10a640cf46
jinWan0823 <dhksl823@gmail.com>
2025-07-04 02:12:16 +0900

    ✨ 로그아웃 추가

--- src/_components/common/AdminBadge.tsx
+++ src/_components/common/AdminBadge.tsx
@@ -1,15 +1,33 @@
 "use client";
 
-import { useSession } from "next-auth/react";
+import { signOut, useSession } from "next-auth/react";
+import { useRouter } from "next/navigation";
 
 export default function AdminBadge() {
   const { data: session } = useSession();
 
+  const router = useRouter();
+
+  const handleLogout = async () => {
+    const logout = confirm("로그아웃 하시겠습니까?");
+
+    // if (logout) {
+    //   signOut({ callbackUrl: "/" });
+    // }
+    if (!logout) return;
+
+    await signOut({ redirect: false });
+    router.push("/");
+  };
+
   if (session?.user) {
     return (
-      <div className="fixed top-[20px] right-[20px] rounded-full px-4 py-1 bg-[#f37812] text-white font-bold border-2 border-white shadow-xl">
-        <span>관리자 접속</span>
-      </div>
+      <button
+        onClick={handleLogout}
+        className="fixed top-[20px] right-[20px] rounded-full px-4 py-1 bg-[#f37812] text-white font-bold border-2 border-white shadow-xl"
+      >
+        관리자 로그아웃
+      </button>
     );
   }
 
